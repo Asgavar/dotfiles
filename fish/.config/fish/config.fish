50654aa30d6c355e95be843bca1906d647bd8c15
Artur Juraszek <asgavar@gmail.com>
2018-02-24 15:10:24 +0100

    hehe polskie znaczki

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,6 +3,7 @@ export XKB_DEFAULT_LAYOUT=pl
 export XKB_DEFAULT_OPTIONS=caps:swapescape
 export XDG_RUNTIME_DIR=/home/asgavar/.xdg
 setxkbmap -option "caps:swapescape"
+setxkbmap pl
 alias usbtether "/home/asgavar/dotfiles/things_void/usb_tether.sh"
 alias dropbox "python2 /home/asgavar/dropbox.py"
 alias gs "git status"

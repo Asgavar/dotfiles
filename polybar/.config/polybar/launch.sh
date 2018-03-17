279040c5ce0d6979b82f7af51e160eb57b5f6772
Artur Juraszek <asgavar@gmail.com>
2018-03-17 18:02:42 +0100

    Yyyyygh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-#killall -q polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
-#while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x wal > /dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
+python -m pywal -R
+
+/usr/local/bin/polybar papiesz
 /usr/bin/polybar papiesz
-#polybar bar2 &
-
-echo "Bars launched..."

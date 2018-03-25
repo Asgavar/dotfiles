c6ca3178d4c069d69a812be3900bb15fa98d58e5
Artur Juraszek <asgavar@gmail.com>
2018-03-25 17:58:06 +0200

    bye emacs

---
+++ emacs/.emacs.d/elpa/ede-php-autoload-20170428.933/ede-php-autoload-pkg.el
@@ -0,0 +1,5 @@
+(define-package "ede-php-autoload" "20170428.933" "Simple EDE PHP Project" 'nil :url "https://github.com/stevenremot/ede-php-autoload" :keywords
+  '("php" "project" "ede"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:

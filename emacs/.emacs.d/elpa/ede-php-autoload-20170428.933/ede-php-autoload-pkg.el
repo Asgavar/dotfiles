a1f92d2a5b8a56c2b05b87971368eef20bc655b7
Artur Juraszek <artur@juraszek.xyz>
2018-04-18 22:00:21 +0200

    Remove .emacs.d

--- emacs/.emacs.d/elpa/ede-php-autoload-20170428.933/ede-php-autoload-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "ede-php-autoload" "20170428.933" "Simple EDE PHP Project" 'nil :url "https://github.com/stevenremot/ede-php-autoload" :keywords
-  '("php" "project" "ede"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
